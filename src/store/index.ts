3470c094960387108975bd55e494831faa536b1a
Anthony Manoras <manorasa@hotmail.com>
2017-06-01 12:44:31 +0100

    Added tests for Vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,18 +5,20 @@ import { Todo } from "@/todos"
 
 Vue.use(Vuex);
 
+export const mutations = {
+  addTodo(state: any, payload: any){
+      let todo = new Todo(state.todoList.length + 1, payload.name, false);
+      state.todoList.push(todo);
+  },
+  toggleStatus(state: any, payload: any) {
+      let index = payload.id - 1;
+      state.todoList[index].done = !state.todoList[index].done;
+  }
+};
+
 export default new Vuex.Store({
   state: {
     todoList: new Array<Todo>()
   },
-  mutations: {
-    addTodo(state, payload){
-        let todo = new Todo(state.todoList.length + 1, payload.name, false);
-        state.todoList.push(todo);
-    },
-    toggleStatus(state, payload) {
-        let index = payload.id - 1;
-        state.todoList[index].done = !state.todoList[index].done;
-    }
-  }
+  mutations
 });
--- test/unit/specs/store/index.spec.ts
+++ test/unit/specs/store/index.spec.ts
@@ -1,4 +1,5 @@
-import store from "@/store";
+import store, { mutations } from "@/store";
+import { Todo } from "@/todos"
 
 describe("Todo Store", () => {
     describe("initialization", () => {
@@ -9,23 +10,55 @@ describe("Todo Store", () => {
         it("should add todoList to state", () => {
             expect(store.state.todoList).toBeDefined();
         });
-
-        it("should add addTodo to mutations", () => {
-            //expect(store.mutations.addTodo).toBeDefined();
-        });
-
-        it("should add toggleStatus to mutations", () => {
-            //expect(store.mutations.toggleStatus).toBeDefined();
-        });
     });
 
     describe("mutations", () => {
-        describe("addTodo", () => {
+        let state: any = {},
+            payload: any;
 
+        describe("addTodo", () => {
+            beforeEach(() => {
+                payload = { name: "Something" };
+                state.todoList = new Array<Todo>();
+            });
+        
+            it("should add a new todo to state.todoList", () => {
+                mutations.addTodo(state, payload);
+
+                expect(state.todoList.length).toBe(1);
+                expect(state.todoList[0]).toBeDefined()
+            });
+
+            it("should set the todo ID", () => {
+                mutations.addTodo(state, payload);
+
+                expect(state.todoList[0].id).toBe(1);
+            });
+
+            it("should set the todo name", () => {
+                mutations.addTodo(state, payload);
+
+                expect(state.todoList[0].id).toBe(1);
+            });
+
+            it("should set the todo done to false", () => {
+                mutations.addTodo(state, payload);
+
+                expect(state.todoList[0].name).toBe(payload.name);
+            });
         });
 
         describe("toggleStatus", () => {
+            beforeEach(() => {
+                payload = { id: 1 };
+                state.todoList = [ new Todo(1, "Something", false) ];
+            });
 
+            it("should reverse the value of done for the todo ID passed", () => {
+                mutations.toggleStatus(state, payload);
+
+                expect(state.todoList[0].done).toBe(true);
+            });
         });
     });
 });
